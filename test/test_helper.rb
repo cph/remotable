e5817167ad0ad952e83346e2ec04b495bec41f13
Bob Lail <bob.lailfamily@gmail.com>
2018-08-14 10:41:07 -0500

    [skip] Used double-quotes and organized test_helper.rb (1m)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,13 @@
-require 'rubygems'
-require 'simplecov'
-require 'active_support/core_ext'
-require 'factory_girl'
-require 'turn'
-require 'pry'
-require 'database_cleaner'
-
-
-require 'active_record'
+require "rubygems"
+require "simplecov"
+require "active_support/core_ext"
+require "factory_girl"
+require "turn"
+require "pry"
+require "database_cleaner"
+require "active_record"
+require "factories/tenants"
+require "minitest/autorun"
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
@@ -15,10 +15,6 @@ ActiveRecord::Base.establish_connection(
   :verbosity => "quiet")
 
 load File.join(File.dirname(__FILE__), "support", "schema.rb")
-
-
-require 'factories/tenants'
-
 
 DatabaseCleaner.strategy = :transaction
 
@@ -30,5 +26,3 @@ class ActiveSupport::TestCase
     DatabaseCleaner.clean
   end
 end
-
-require "minitest/autorun"
